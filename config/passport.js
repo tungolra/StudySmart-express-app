36042d4bf4f547d61cbe9b1492e17a0a640b10e5
rtungol <rarrtungol@gmail.com>
2022-10-22 11:53:46 -0400

    added serialize/deserialize methods to passport.js

--- config/passport.js
+++ config/passport.js
@@ -30,3 +30,12 @@ passport.use(
     }
   )
 );
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
