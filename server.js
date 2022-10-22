9ad84faa4b45a80b2067c92fb346b018a544951e
rtungol <rarrtungol@gmail.com>
2022-10-22 11:33:46 -0400

    session middleware now in place

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+const session = require("express-session");
 require("dotenv").config();
 
 require("./config/database");
@@ -18,6 +18,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+  session({
+    secret: "StudySmart!",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/home"));
