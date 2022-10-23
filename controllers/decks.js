050d451a28c12393a870a0a8f98d501998eb7053
rtungol <rarrtungol@gmail.com>
2022-10-23 18:15:37 -0400

    added create new deck function

--- controllers/decks.js
+++ controllers/decks.js
@@ -9,20 +9,19 @@ function newDeck(req, res) {
 }
 function create(req, res) {
   //create function
-//   User.findById(req.params.id, function (err, user) {
-    // if (err) return res.send(err.message);
-    console.log(req.user)
-    // user.decks.push(req.body);
-    // deck.save(function (err) {
-    //   if (err) return res.redirect("decks/new-deck");
+  User.find(req.user, function (err, user) {
+    if (err) return res.send(err.message);
+    req.user.decks.push(req.body);
+    req.user.save(function (err) {
+      if (err) return res.send(err.message);
       res.redirect("/summary");
-    // });
-//   });
+    });
+  });
 }
-function show (req, res) {
-    //find by ID
-    console.log(req.user)
-    res.render(`decks/show-deck`)
+function show(req, res) {
+  //find by ID
+  // console.log(req.user)
+  res.render(`decks/show-deck`);
 }
 
 module.exports = {
