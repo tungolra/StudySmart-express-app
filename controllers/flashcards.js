87811170fba23279fd638db5013882433d4c5f7a
rtungol <rarrtungol@gmail.com>
2022-10-24 10:33:27 -0400

    last page to revert to before making new flashcard functions

--- controllers/flashcards.js
+++ controllers/flashcards.js
@@ -1,14 +1,10 @@
 const User = require("../models/user");
 
 function newCard(req, res) {
-  //   User.find(req.user, function (err, user) {
-  //     if (err) return res.send(err.message);
-  //     //how do i pass in the specific deck i want to push the new flashcard to
-  //     console.log(req.params.id);
-  console.log();
   res.render("flashcards/new-card");
-  //   });
+
 }
+
 module.exports = {
   newCard,
 };
