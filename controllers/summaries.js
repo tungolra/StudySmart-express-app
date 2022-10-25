273071aeb25d6f30c5d0a12f3287ce105b7f34ca
rtungol <rarrtungol@gmail.com>
2022-10-25 19:09:10 -0400

    added api folders

--- controllers/summaries.js
+++ controllers/summaries.js
@@ -5,7 +5,6 @@ function summary(req, res) {
   User.findById(req.params.id, function (err, user) {
     if (err) return res.send(err.message);
     let decks = req.user.decks;
-    console.log(decks)
     res.render("summary/summary.ejs", { decks });
   });
 }
--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,2 +1,13 @@
-// const BASE_URL=
+const BASE_URL="mongodb://127.0.0.1:27017/Flashcards"
 
+// let users, decks, flashcards;
+getData()
+async function getData() {
+  try {
+    const endpoint = "http://localhost:1100/summary"
+    let users = await fetch(endpoint).then(res => res.json())
+    console.log(users);
+  } catch (err) {
+    console.log(err);
+  }
+}
